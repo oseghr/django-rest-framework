a4d8304abda6d92fc5884a5e8b4454446ee1325a
Ose Benson <o.oaikhena@yahoo.com>
2022-07-02 16:49:19 -0600

    Updated the model

---
+++ venv/I4G031075DBC/links/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.forms import BooleanField
+
+# Create your models here.
+
+class Links(models.Model):
+    target_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(max_length=20, blank=True, unique=True)
+    # author = get_user_model.ForeignKey() : A Foreign Key to the current user model. Make use of Django’s function.
+    created_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True) 
+
+    def __str__(self):
+        return self.target_url
